73f09c525c25227043d7c2c9ac5b8e05d82ef2c1
Francisco Fierro <f.a.fierrojr@gmail.com>
2016-10-08 20:46:05 -0700

    Prob 3 code part fini.

---
+++ proj3/prob3.m
@@ -0,0 +1,19 @@
+clear;
+clc;
+
+A = [0,1,2,3,4,5,6]
+B =[32,47,65,92,132,190,275]
+
+plot(A,B,'o')
+
+[P]=polyfit(A,log(B),1)
+
+b=P(1)
+a=exp(P(2))
+func =@(x)(a*exp(b*x))
+
+hold on
+
+fplot(func,[0,6])
+
+hold off
